da2435698c3f92bb57d8776a17686b1f2ca09272
clayton-schneider <schneiderc2016@gmail.com>
2023-07-26 22:18:40 -0400

    updated image entry point

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,6 +10,9 @@ import react from "@astrojs/react";
 export default defineConfig({
   experimental: { assets: true },
   site: "https://sitegoeshere.com",
+  image: {
+    service: sharpImageService(),
+  },
   integrations: [
     tailwind({
       config: {
