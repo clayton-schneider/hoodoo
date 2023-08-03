7986d7e879d97f09974bb756e6165656167af5d1
clayton-schneider <schneiderc2016@gmail.com>
2023-08-03 12:46:30 -0400

    added robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig, sharpImageService } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  experimental: { assets: true },
-  site: "https://sitegoeshere.com",
+  experimental: {
+    assets: true,
+  },
+  site: "https://www.sitegoeshere.com",
   image: {
     service: sharpImageService(),
   },
@@ -20,5 +24,8 @@ export default defineConfig({
       },
     }),
     react(),
+    robotsTxt({
+      policy: [{ userAgent: "*", disallow: "/assets/*" }],
+    }),
   ],
 });
